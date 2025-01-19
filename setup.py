097d1c29d5579992d2d77b95cfd1cc3eee7d8e2b
garimaahuja112 <garimaahuja217@gmail.com>
2025-01-19 23:08:07 +0530

    released new version

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ README = (HERE / "README.md").read_text()
 # This call to setup() does all the work
 setup(
     name="Topsis-Garima-102203385",
-    version="1.0.0",
+    version="1.0.1",
     description="Perform TOPSIS (Technique for Order Preference by Similarity to Ideal Solution) analysis on a dataset.",
     long_description=README,
     long_description_content_type="text/markdown",
